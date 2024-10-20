c82c93debba46d8d8fa7283a4d2fa3b540c4b480
Trickster[;] <90096017+TricksterX1337@users.noreply.github.com>
2024-10-20 20:49:42 +0530

    Update commandLoader.js

--- handler/commandLoader.js
+++ handler/commandLoader.js
@@ -4,10 +4,8 @@ const path = require('path');
 function loadCommands() {
     const commands = new Map();
     
-    // Assuming your commands are in a separate folder like `/commands`
     const commandFolder = path.join(__dirname, '../commands');
     
-    // Read all JavaScript files from the `/commands` folder
     const commandFiles = fs.readdirSync(commandFolder).filter(file => file.endsWith('.js'));
 
     // Loop through each command file and add it to the commands map
